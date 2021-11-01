0f62fa9d7be320d0c6499a6cddf0c93316b45bb4
patel-prit <prit999@users.noreply.github.com>
2021-11-02 01:01:43 +0530

    fix issue

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Post.css";
 import { Link } from "react-router-dom";
 
-function Post({ posts, loading }) {
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
+function Post({ posts }) {
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [posts]);
   return (
     <div className="data-list">
       <ul className="list-group ">
         {posts.map((post) => (
           <div key={post.id} className="media post-container">
-            <Link to={`/${posts}/${post.id}`} target="_blank">
+            <Link to={`/articles/${post.id}`}>
               <img
                 src={post.imageUrl}
                 className="rounded float-left post-image"
@@ -19,7 +19,7 @@ function Post({ posts, loading }) {
               />
             </Link>
             <div className="media-body post-container-detail">
-              <Link to={`/articles/${post.id}`} target="_blank">
+              <Link to={`/articles/${post.id}`}>
                 <h5 className="mt-0">{post.title}</h5>
               </Link>
               <p>News Site : {post.newsSite}</p>
